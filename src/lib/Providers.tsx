e57b632eb66e0ee3979991e2f12922d4df448f45
mazdul1000 <mazdul1000@gmail.com>
2023-12-27 18:58:00 +0600

    [DONE]MH-36: Configuring Ant design

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -2,10 +2,13 @@
 
 import { store } from "@/redux/store";
 import { Provider } from "react-redux";
+import StyledComponentsRegistry from "./AntdRegistry";
 
 const Providers = ({children}: { children: React.ReactNode}) => {
   return <Provider store={store}>
+    <StyledComponentsRegistry>
     {children}
+    </StyledComponentsRegistry>
   </Provider>
 };
 
