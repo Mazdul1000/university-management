42f892d3c229c7e5c5fc1e1028f90a6cbc4d8353
mazdul1000 <mazdul1000@gmail.com>
2023-12-28 13:04:45 +0600

    [DONE]MH-45: Create resuable BreadCrumbs

--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -2,14 +2,27 @@
 
 import React from 'react';
 import {  Layout } from 'antd';
+import UMBreadCrumbs from './UMBreadCrumbs';
 const { Content } = Layout;
 
 const Contents = ({children}:{children:React.ReactNode}) => {
+
+    const base = "admin"
     return (
         <Content style={{
             minHeight: "100vh",
             color:"black"
         }}>
+            <UMBreadCrumbs items={[
+                {
+                    label: `${base}`,
+                    link: `/${base}`
+                },
+                {
+                    label: "students",
+                    link: `/${base}/student`
+                }
+            ]}/>
             {children}
         </Content>
     );
