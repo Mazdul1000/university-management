c73033c99b306e4ee041cc1a208b3731832617b8
mazdul1000 <mazdul1000@gmail.com>
2023-12-28 12:07:58 +0600

    MH-43: Create login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+"use client";
+
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
+import React from "react";
+import loginImage from "../../assets/login-img.png";
+import Form from "@/components/Forms/Form";
+import { SubmitHandler } from "react-hook-form";
+import FormInput from "@/components/Forms/FormInput";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
 
 const LoginPage = () => {
-    return (
+  const onsubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (err) {}
+  };
+  return (
+    <Row>
+      <Col sm={12} md={16} lg={16}>
+        <Image src={loginImage} width={500} alt="login-image" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1>First login your account</h1>
         <div>
-            <h1>This is Login Page</h1>
+          <Form submitHandler={onsubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User ID" />
+            </div>
+            <div>
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+                Login
+            </Button>
+          </Form>
         </div>
-    );
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginPage;
